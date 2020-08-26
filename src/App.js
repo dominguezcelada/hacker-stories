24fd7f2d1e25f86d7738d39c9df5ec961256cb80
Oscar Dominguez <dominguez.celada@gmail.com>
2020-08-26 21:18:01 +0200

    feat(react-jsx): use a variable to display under h1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './App.css';
 
+const title = 'React';
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {title}</h1>
     </div>
   );
 }
