03366358d8312e21008508e9db816ec58d9d0486
OscarDOM <dominguez.celada@gmail.com>
2020-08-26 21:36:42 +0200

    feat(lists-in-react): print lists in react (#4)

--- src/App.js
+++ src/App.js
@@ -2,20 +2,53 @@ import React from 'react';
 
 import './App.css';
 
-const messages = [
-  'amo',
-  'niño',
-  '!!!'
-]
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Vue',
+    url: 'https://vuejs.org/',
+    author: 'Evan You',
+    num_comments: 3,
+    points: 4,
+    objectID: 1,
+  },
+  {
+    title: 'Svelte',
+    url: 'https://svelte.dev/',
+    author: 'Rich Harris',
+    num_comments: 3,
+    points: 4,
+    objectID: 2,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 3,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>
-        {messages.join(' ')}
-      </h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+      <hr />
+      { list.map(item => {
+        return <div key={item.objectID}>
+          {item.title}
+        </div>
+      })}
     </div>
   );
 }
