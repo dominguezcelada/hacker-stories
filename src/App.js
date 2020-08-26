70b1c3c4d2cfe3262f279fc155473390142ccb46
Oscar Dominguez <dominguez.celada@gmail.com>
2020-08-26 21:21:14 +0200

    feat(react-jsx): using object properties to fulfill h1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import './App.css';
 
-const title = 'React';
+const { greeting, title } = {
+  greeting: 'Hey',
+  title: 'React',
+};
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        {greeting} {title}
+      </h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
