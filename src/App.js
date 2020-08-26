0b8bb1c686953815bb06c5093df2d6def5f66b1a
Oscar Dominguez <dominguez.celada@gmail.com>
2020-08-26 21:31:38 +0200

    feat(lists-in-react): print lists in react

--- src/App.js
+++ src/App.js
@@ -2,20 +2,37 @@ import React from 'react';
 
 import './App.css';
 
-const messages = [
-  'amo',
-  'niño',
-  '!!!'
-]
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>
-        {messages.join(' ')}
-      </h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+      <hr />
+      { list.map(item => {
+        return <div>
+          {item.title}
+        </div>
+      })}
     </div>
   );
 }
