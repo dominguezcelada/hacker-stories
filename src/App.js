0d6ee213514b9042310a6e57b0dbf45d7c32d960
OscarDOM <dominguez.celada@gmail.com>
2020-09-30 17:30:10 +0200

    feat(react-component-instantiation): create <List> component (#5)

--- src/App.js
+++ src/App.js
@@ -37,20 +37,20 @@ const list = [
   },
 ];
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-      <hr />
-      { list.map(item => {
-        return <div key={item.objectID}>
-          {item.title}
-        </div>
-      })}
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+    <hr />
+    <List/>
+  </div>
+);
+
+const List = () => (
+  <div>
+    { list.map(item => <div>{item.title}</div>) }
+  </div>
+);
 
 export default App;
